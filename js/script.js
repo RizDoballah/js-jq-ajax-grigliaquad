06b8d2843248b5be18e9cb3bebb3ca93a05a8e1a
Ghezlene Doballah <ghyzounette@gmail.com>
2020-01-30 18:00:55 +0100

    final commit

--- js/script.js
+++ js/script.js
@@ -3,42 +3,58 @@
 // se è > di 5 il quadrato diventa verde.
 // Il numero ottenuto appare al centro del quadrato.
 
+// $(document).ready(function() {
+//   $('.box').click(function() {
+//     var box = $(this);
+//     console.log(box);
+//     $.ajax(
+//       {
+//         url: "https://flynn.boolean.careers/exercises/api/random/int",
+//         method: "GET",
+//         success: function (data) {
+//           console.log(box);
+//           console.log(data.response);
+//             if (data.response <= 5) {
+//               box.addClass('yellow');
+//             } else if (data.response > 5) {
+//               box.addClass('green');
+//             }
+//             box.text(data.response);
+//         },
+//         error: function () {
+//           alert("E' avvenuto un errore. ");
+//         }
+//       }
+//     );
+//
+//   });
+// });
 $(document).ready(function() {
-  $('.box').click(function() {
+  for (var i = 0; i < 36; i++) {
+    $('<div class="box"><h3></h3></div>').appendTo($('#row'))
+  }
+  $(document).on('click', '.box', function () {
     var box = $(this);
-    console.log(box);
     $.ajax(
-      {
-        url: "https://flynn.boolean.careers/exercises/api/random/int",
-        method: "GET",
-        success: function (data) {
-          console.log(box);
-          console.log(data.response);
+        {
+          url: "https://flynn.boolean.careers/exercises/api/random/int",
+          method: "GET",
+          success: function (data) {
+            console.log(box);
+            console.log(data.response);
             if (data.response <= 5) {
               box.addClass('yellow');
-            } else if (data.response > 5) {
-              box.addClass('green');
+              } else if (data.response > 5) {
+                box.addClass('green');
+              }
+              box.text(data.response);
+            },
+            error: function () {
+              alert("E' avvenuto un errore. ");
             }
-            box.text(data.response);
-        },
-        error: function () {
-          alert("E' avvenuto un errore. ");
-        }
-      }
-    );
+          }
+        );
 
   });
 
-
-  // for (var i = 0; i < 36; i++) {
-  //   $('#row').append($('.box'))
-  // }
-  // var source = $('.#entry-template').html();
-  // var template = Handlebars.compile(source);
-  //
-  // var context = {
-  //   'number': data.response,
-  // };
-  // var html = template(context);
-  // $('#row').append(html);
 });
